e62de10e305ebf4aaa913939845e377826ebab0b
halfss <ljvsss@gmail.com>
2013-12-18 15:14:39 +0800

    add vim auto complate

--- setup.sh
+++ setup.sh
@@ -10,8 +10,9 @@ if [ ! -d "bundle" ]; then
     mkdir bundle
 fi
 
-wget http://www.vim.org/scripts/download_script.php?src_id=14366 -o bundle/pep8.vim
+wget http://www.vim.org/scripts/download_script.php?src_id=14366 -O bundle/pep8.vim
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+git clone https://github.com/rkulla/pydiction.git bundle/pydiction
 
 cat > ~/.vimrc << EOF
 syntax on
@@ -30,6 +31,10 @@ filetype plugin indent on
 execute pathogen#infect()
 
 map <C-A> ggVG
+
+filetype plugin on
+let g:pydiction_location = '~/.vim/bundle/pydiction/complete-dict'
+let g:pydiction_menu_height = 20 
 EOF
 
 cd -
